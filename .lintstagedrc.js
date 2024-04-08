8e97a51965d8cc878c4310155e967aefcbbc9938
Rachit Patel <rachit.patel@krishtechnolabs.com>
2024-04-08 12:26:20 +0530

    fix: [CT-123] Fixed lint staged issue

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   // This will lint and format TypeScript and JavaScript files
-  '**/*.(json,ts,md)': [`prettier --write `, `eslint --fix`],
+  '*.{json,ts,md}': [`prettier --write `, `eslint --fix`],
 }
